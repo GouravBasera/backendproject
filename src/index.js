f20f3ed319cdf72178df27cf2238ff865450c47c
Gourav Basera <gouravbasera06@gmail.com>
2024-09-18 21:21:01 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
 connectDB()
+  .then(() => {
+    app.on('error', (error) => {
+        console.log(error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection failed", err);
+  });
